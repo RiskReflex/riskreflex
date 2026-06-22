b277f670ee265fd26627796ec6badb3530cd1067
RiskReflex <labs@riskreflex.in>
2026-06-22 20:19:16 +0530

    Update orb movement calculations and styles

--- js/orb.js
+++ js/orb.js
@@ -2,12 +2,15 @@ const orb = document.querySelector('.orb-core');
 
 document.addEventListener('mousemove', (e) => {
 
-    const x =
-        (window.innerWidth / 2 - e.pageX) / 40;
+```
+const x =
+    (window.innerWidth / 2 - e.clientX) / 60;
 
-    const y =
-        (window.innerHeight / 2 - e.pageY) / 40;
+const y =
+    (window.innerHeight / 2 - e.clientY) / 60;
 
-    orb.style.transform =
-        `translate(${x}px, ${y}px)`;
+orb.style.marginLeft = `${-x}px`;
+orb.style.marginTop = `${-y}px`;
+```
+
 });
